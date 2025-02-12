552188dc707f6cb9815276ee56652c46692db918
sadinrahman <sadinsadinrahman@gmail.com>
2025-02-12 13:27:53 +0530

    Time: 0 ms (100%), Space: 44.6 MB (77.78%) - LeetHub

---
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int DiagonalSum(int[][] mat) {
+        int sum=0;
+        for(int i=0;i<mat.Length;i++){
+           sum+=mat[i][i];
+           if (i != mat.Length - i - 1) {
+                sum += mat[i][mat.Length - i - 1];
+            } 
+        }
+        return sum;
+    }
+}
