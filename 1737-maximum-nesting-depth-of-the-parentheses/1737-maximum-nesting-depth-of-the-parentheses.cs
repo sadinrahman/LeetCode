edec9f69e023524c3156148d392c7f699f54f1f0
sadinrahman <sadinsadinrahman@gmail.com>
2025-02-14 09:46:22 +0530

    Time: 0 ms (100%), Space: 38.6 MB (52.1%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MaxDepth(string s) {
+        int maxDepth = 0;
+        int currentDepth = 0;
+
+        foreach (char c in s) {
+            if (c == '(') {
+                currentDepth++;
+                maxDepth = Math.Max(maxDepth, currentDepth);
+            } else if (c == ')') {
+                currentDepth--;
+            }
+        }
+
+        return maxDepth;
+    }
+}
